22efdb0a16f5685d32bc94500621a1d5bd15be69
alex <ale06.angeletti@icloud.com>
2026-02-26 19:25:18 +0100

    Creato classe Book e Librari, inserimento di libri e stampa totale

--- lezione0/bookV1.py
+++ lezione0/bookV1.py
@@ -2,16 +2,33 @@ class Book:
     def __init__(self, titolo, autore, anno, tipo):
         self.titolo = titolo
         self.autore = autore
-        self.autore = anno
+        self.anno = anno
         self.tipo = tipo
-    def str(self):
-        return f"{self.titolo} | {self.autore} | {self.anno} | {self.tipo}"
+    def stampa(self):
+        return f"{self.titolo} | {self.autore} | {self.anno} | {self.tipo}"  
+      
 class Library:
-    def __init__(self, libro):
-        self.libro = []
+    def __init__(self):
+        self.libri = []
 
     def aggiungi_libro(self, libro):
-        self.libro.apend(libro)
+        self.libri.append(libro)
+    def mostra_libri(self):
+        for l in self.libri:
+            print(l.stampa())
 
-library = Library()
-        
+
+letto = "-"
+biblioteca  = Library()
+while(letto != "quit"):
+    titolo = input("Inserisci il titolo del libro: ")
+    autore = input("Inserisci l'autore del libro: ")
+    anno = input("Inserisci l'anno di pubblicazione del libro: ")
+    tipo = input("Inserisci il tipo del libro (es. romanzo, saggio, ecc.): ")
+
+    libro = Book(titolo, autore, anno, tipo)
+    biblioteca.aggiungi_libro(libro)
+
+    letto = input("Vuoi inserire un altro libro? (digita 'quit' per uscire): ")
+
+biblioteca.mostra_libri()
