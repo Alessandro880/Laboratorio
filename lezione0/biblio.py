26bef61f8e129d453d648b839c0dbbcc5d0fbc42
alex <ale06.angeletti@icloud.com>
2026-02-27 20:11:44 +0100

    Versione finita con confronto tempo

--- lezione0/biblio.py
+++ lezione0/biblio.py
@@ -1,21 +1,30 @@
 from book import Book
 from library import Library
 from libraryFast import LibraryFast
-
+import time
 
 #biblioteca = Library()
 biblioteca = LibraryFast()
 
-for i in range(100000):
+for i in range(10000000):
     libro = Book(f"{i}", f"{i}", 2000+i, f"{i%10}")
     biblioteca.aggiungi_libro(libro)
 
-biblioteca.mostra_libri()
 
 print("\n\nRicerca per autore\n")
 cerca_a = input("Inserisci autore da cercare : ")
 
+inizio = time.perf_counter()
 biblioteca.cerca_autore(cerca_a)
+fine = time.perf_counter()
+tempo = fine-inizio
+print(f"Tempo impiegato per cercare autore : {tempo:.8f} secondi\n")
+
 print("\n\nRicerca per titolo\n")
 cerca_t = input("Inserisci titolo da cercare : ")
+
+inizio = time.perf_counter()
 biblioteca.cerca_titolo(cerca_t)
+fine = time.perf_counter()
+tempo = fine-inizio
+print(f"Tempo impiegato per cercare titolo : {tempo:.8f} secondi\n")
